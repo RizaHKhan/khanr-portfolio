0f2cbe2f5b6bc3f03916da2c6aff0df446acc6bc
RizaHKhan <khanriza@gmail.com>
2020-04-13 23:46:36 -0400

    Implemented view and controller to add new portfolio items

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -4,4 +4,22 @@ class PortfoliosController < ApplicationController
     @portfolio_items = Portfolio.all
   end
 
+  # The new method refers to creating a new portfolio
+  def new
+    @portfolio_item = Portfolio.new
+  end
+
+  def create
+    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
+
+    respond_to do |format|
+      if @portfolio_item.save
+        # Per routes, if we don't want to send the user to the newly created portfolio item,we can send them back to the archive page.
+        format.html { redirect_to @portfolios_path, notice: 'New Portfolio was successfully created.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
 end
