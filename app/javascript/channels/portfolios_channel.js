5136842c7f5c2c5b1281df67e5c6b7d2780c5cae
RizaHKhan <khanriza@gmail.com>
2020-04-27 00:13:18 -0400

    Some draggable logic completed

--- app/javascript/channels/portfolios_channel.js
+++ app/javascript/channels/portfolios_channel.js
@@ -1,5 +1,29 @@
 import Sortable from "sortablejs";
 document.addEventListener("DOMContentLoaded", () => {
-  var sortableItem = document.querySelector(".sortme");
+  const sortableItem = document.querySelector(".sortable");
+  const card = document.querySelectorAll(".portfolio-item");
+
+  card.forEach((e) => {
+    e.addEventListener("drop", () => {
+      set_position(card);
+    });
+  });
+
+  set_position();
   var sortable = Sortable.create(sortableItem);
 });
+
+function set_position() {
+  let x = 1;
+  const card = document.querySelectorAll(".portfolio-item");
+  card.forEach((e) => {
+    e.setAttribute("data-pos", x);
+    console.log(
+      "data-id:",
+      e.getAttribute("data-id"),
+      "data-pos:",
+      e.getAttribute("data-pos")
+    );
+    x++;
+  });
+}
