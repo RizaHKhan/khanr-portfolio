537c97e8a203abcdbd6abec02fed46af05d05311
RizaHKhan <khanriza@gmail.com>
2020-05-01 00:07:54 -0400

    added desc order to blog posts

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,9 +1,11 @@
 class Blog < ApplicationRecord
-  belongs_to :topic
+  belongs_to :topic, optional: true
   # Tell rails what attribute is the enum, and give it an object with what the "word" the number refers to.
   enum status: { draft: 0, published: 1 }
   extend FriendlyId
   friendly_id :title, use: :slugged
 
   validates_presence_of :title, :body
+
+  default_scope {order(created_at: :desc)}
 end
