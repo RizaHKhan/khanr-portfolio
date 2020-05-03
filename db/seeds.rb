62dc785223fb0827f13be5882505ec8d8686ec60
RizaHKhan <khanriza@gmail.com>
2020-05-02 21:58:20 -0400

    added carousel partial and added three images to it

--- db/seeds.rb
+++ db/seeds.rb
@@ -31,6 +31,8 @@ puts '5 Skills Created'
     subtitle: "Ruby on Rails",
     body: "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.",
     main_image: 'https://place-hold.it/550',
+    second_image: 'https://place-hold.it/550',
+    third_image: 'https://place-hold.it/550',
     thumbnail_image: 'https://place-hold.it/100'
   )
 end
@@ -43,6 +45,8 @@ puts '8 Portfolio Items Created'
     subtitle: "VueJS",
     body: "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.",
     main_image: 'https://place-hold.it/550',
+    second_image: 'https://place-hold.it/550',
+    third_image: 'https://place-hold.it/550',
     thumbnail_image: 'https://place-hold.it/100'
   )
 end
