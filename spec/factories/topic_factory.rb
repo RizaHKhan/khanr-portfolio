94ac31baa4a6f0ae1f30648b260e0e7b9bdd6844
RizaHKhan <khanriza@gmail.com>
2020-05-10 00:51:59 -0400

    finalized blog model tests

---
+++ spec/factories/topic_factory.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :topic do
+    sequence(:title) { |n| "Title#{n}" }
+  end
+end
--- spec/models/blogs_spec.rb
+++ spec/models/blogs_spec.rb
@@ -5,5 +5,26 @@ RSpec.describe Blog, type: :model do
     it 'should create a blog' do
       expect(build :blog, topic_id: 1).to be_valid
     end
+
+    it 'should fail to create a blog with missing title' do
+      expect(build :blog, topic_id: 1, title: '').not_to be_valid
+    end
+
+    it 'should fail to create a blog with missing body' do
+      expect(build :blog, topic_id: 1, body: '').not_to be_valid
+    end
+
+    it 'should fail to create a blog with missing topic_id' do
+      expect(build :blog).not_to be_valid
+    end
+  end
+
+  describe '#methods' do
+    it 'should show the last blog first per default scope' do
+      topic = create :topic
+      blog1 = create :blog, topic_id: topic.id
+      blog2 = create :blog, topic_id: topic.id
+      expect(Blog.first.id).to eq(blog2.id)
+    end
   end
 end
